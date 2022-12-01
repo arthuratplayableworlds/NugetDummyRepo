2f5457a0ee7d8f693b5c0d4933d4a3d1b2be9e84
Arthur <arthur.littlefield@playableworlds.com>
2022-12-01 13:15:56 -0800

    - add project.

---
+++ NugetDummyRepo/NugetDummyRepo/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace NugetDummyRepo;
+
+public class Class1
+{
+    public static void PrintStuff()
+    {
+        var assembly = Assembly.GetAssembly(typeof(Class1));
+        var version = FileVersionInfo.GetVersionInfo(assembly.Location).ProductVersion;
+        Console.WriteLine($"This is the dummy repo version : {version}.");
+    }
+}
