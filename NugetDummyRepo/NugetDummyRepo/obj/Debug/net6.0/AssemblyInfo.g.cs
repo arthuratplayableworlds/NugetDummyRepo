4e2b14674d4798eae48d519746a7642e7e634787
Arthur <arthur.littlefield@playableworlds.com>
2022-12-01 17:05:53 -0800

    minor change

--- NugetDummyRepo/NugetDummyRepo/obj/Debug/net6.0/AssemblyInfo.g.cs
+++ NugetDummyRepo/NugetDummyRepo/obj/Debug/net6.0/AssemblyInfo.g.cs
@@ -8,6 +8,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyFileVersion("2.1.0.0")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyInformationalVersion("2.1.0-jenkins-ci.2+Branch.jenkins-ci.Sha.633e289fd759fd24f6ca7337ff76fa0bc795159f")]
+[assembly: AssemblyFileVersion("2.1.1.0")]
+[assembly: AssemblyVersion("2.1.1.0")]
+[assembly: AssemblyInformationalVersion("2.1.1+Branch.main.Sha.75e8b22c483d34ead9e58ffa1be67c2bed4a371a")]
